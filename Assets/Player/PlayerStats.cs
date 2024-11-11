a3a2a6b7f4c8b661e1279862171980a8fcc03a2f
EmilEkroth <emil@ekroth.se>
2024-11-11 13:47:46 +0200

    Rebalanced red

--- Assets/Player/PlayerStats.cs
+++ Assets/Player/PlayerStats.cs
@@ -26,7 +26,7 @@ public class PlayerStats : MonoBehaviour
     public float colorRainbowMaxedPower = 1;
     
     int shield = 0;
-    int shieldDecay = 0;
+    int shieldDecay = -1;
 
     public int chanceThatEnemyDontMix = 0;
 
@@ -116,6 +116,7 @@ public class PlayerStats : MonoBehaviour
     {
         if(invincibilityTimer > 0) return;
         //if(damage == 0) return;
+        shieldDecay = 0;
         if (UnityEngine.Random.Range(0, 100) < chanceToBlock)
         {
             GameObject aura = Instantiate(blockAura, transform);
