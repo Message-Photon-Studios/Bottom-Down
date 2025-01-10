ea0d777acfcc9bc879239a1aa22c44f24d379918
Samuel Dahlborg <beaosam@gmail.com>
2025-01-10 15:42:15 +0100

    Added Soundmixer and Mainmenu settings
    Added soundmixer to handle different types of sounds.
    Added settings page to mainmenu that allows tweaking of soundvolume.
    Saves sound settings between runs

--- Assets/GameManager/SettingsManager.cs
+++ Assets/GameManager/SettingsManager.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
 using UnityEngine.Events;
+using UnityEngine.Audio;
 
 public class SettingsManager : MonoBehaviour
 {
-
+    [SerializeField] private AudioMixer mainAudioMixer;
+    [SerializeField] private String[] soundGroups;
     private int selectedPixelSize = 100;
-
 
     private void Awake()
     {
         DontDestroyOnLoad(this);
+    }
+
+    private void Start() {
+        foreach(string group in soundGroups) {
+            if (PlayerPrefs.HasKey(group)) {
+                LoadVolume(group);
+            } else {
+                SetVolume(1, group);
+            }
+        }
+    }
+    
+    public void SetVolume(float volume, string group) {
+        mainAudioMixer.SetFloat(group, Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat(group, volume);
+    }
+
+    private void LoadVolume(string group) {
+        float volume = PlayerPrefs.GetFloat(group);
+        SetVolume(volume, group);
     }
 }
 
