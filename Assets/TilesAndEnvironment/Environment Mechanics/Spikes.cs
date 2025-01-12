c599d78c7133365e71353371c9878f8694229356
EmilEkroth <emil@ekroth.se>
2025-01-12 18:13:07 +0200

    Fixed spikes

---
+++ Assets/TilesAndEnvironment/Environment Mechanics/Spikes.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Spikes : MonoBehaviour
+{
+    [SerializeField] int damage;
+    [SerializeField] float damageTimer = 1f;
+    float damageTime = 0;
+    bool playerInSpikes = false;
+    PlayerStats playerStats;
+    PlayerMovement playerMovement;
+    void Start()
+    {
+        playerStats = PlayerLevelMananger.instance.playerStats;
+        playerMovement = PlayerLevelMananger.instance.playerMovement;
+    }
+
+    void Update()
+    {
+        if(playerInSpikes)
+        {
+            damageTime -= Time.deltaTime;
+            if(damageTime < 0)
+            {
+                damageTime = damageTimer;
+                playerStats.DamagePlayer(damage, null);
+                playerMovement.movementRoot.SetRoot("spikes", .5f);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            damageTime = 0;
+            playerInSpikes = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInSpikes = false;
+        }
+    }
+}
