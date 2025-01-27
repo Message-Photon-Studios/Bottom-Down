91095f4c50678f0d40715551faf3b2563c4d783b
EmilEkroth <emil@ekroth.se>
2025-01-27 16:29:14 +0200

    Setting up floating crystal

---
+++ Assets/Enemies/Scripts/FloatingCrystal.cs
@@ -0,0 +1,63 @@
+using BehaviourTree;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FloatingCrystal : Enemy
+{
+    [SerializeField] float turnSpeed;
+    [SerializeField] float passiveViewRange;
+    [SerializeField] float aggressiveMaxDistance;
+    [SerializeField] float attackDistance;
+    [SerializeField] Quaternion startRotation;
+    [SerializeField] GameObject attackPrefab;
+    protected override Node SetupTree()
+    {
+        Node root = new Selector(new List<Node>{
+
+            new Sequence(new List<Node>{
+                new CheckBool("aggressive", false),
+                new CheckPlayerDistance(stats, player, 0, passiveViewRange),
+                new SetParentVariable("aggressive", true, 2)
+            }),
+
+            new Sequence(new List<Node>{
+                new CheckBool("aggressive", true),
+                new Inverter(new CheckPlayerDistance(stats, player, 0, aggressiveMaxDistance)),
+                new Wait(3, 1),
+                new SetParentVariable("aggressive", false, 2)
+            }),
+
+            new Sequence(new List<Node>{
+                new CheckBool("aggressive", true),
+                new LookAtPlayer(stats, player),
+                new HomTowardsTarget(stats, startRotation, player.transform, 1f, turnSpeed, .5f),
+                new CheckPlayerDistance(stats, player, 0, attackDistance),
+                new Wait(.3f),
+                new EnemyObjectSpawner(stats, attackPrefab, Vector2.zero, Vector2.right, true, 1)
+            })
+        });
+
+        root.SetData("aggressive", false);
+        return root;
+    }
+
+    protected override void DamageTaken(float damage, Vector2 atPosition)
+    {
+        if(root != null)
+            root.SetData("aggressive", true);
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(stats.GetPosition(), Vector3.forward, attackDistance);
+        Handles.color = Color.blue;
+        Handles.DrawWireDisc(stats.GetPosition(), Vector3.forward, passiveViewRange);
+        Handles.color = Color.yellow;
+        Handles.DrawWireDisc(stats.GetPosition(), Vector3.forward, aggressiveMaxDistance);
+    }
+#endif
+}
