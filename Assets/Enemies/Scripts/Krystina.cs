f86ec2f9a6442c6b4c7d4eb2cf9f14913a8acbb7
EmilEkroth <emil@ekroth.se>
2025-02-19 18:02:34 +0200

    Fixed all but crawler enemies

--- Assets/Enemies/Scripts/Krystina.cs
+++ Assets/Enemies/Scripts/Krystina.cs
@@ -6,14 +6,32 @@ using UnityEditor;
 
 public class Krystina : Enemy
 {
+    [SerializeField] int damage;
+    [SerializeField] Trigger attackTrigger;
     [SerializeField] float patrollIdleTime;
 
     protected override Node SetupTree()
     {
         
-        Node root = new RandomPatroll(stats, body, animator, 1, patrollIdleTime, .4f, "attack", "walk");
+        Node root = new Selector(new List<Node>{
+            new Sequence(new List<Node>{
+                new CheckPlayerArea(stats, player, attackTrigger),
+                new AnimationBool(animator, "walk", false),
+                new DamagePlayer(stats, player, damage)
+            }),
+
+            new RandomPatroll(stats, body, animator, 1, patrollIdleTime, .4f, "attack", "walk"),
+        });
+        
         
         root.SetData("attack", false);
+        triggersToFlip.Add(attackTrigger);
         return root;
     }
+
+    #if UNITY_EDITOR
+    private void OnDrawGizmosSelected() {
+        attackTrigger.DrawTrigger(stats.GetPosition());
+    }
+    #endif
 }
